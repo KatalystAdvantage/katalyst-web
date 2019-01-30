30a4a701b6a1712fe1f25d8c85abff04d0c42f0b
Kelly Knights <kknights@katalystadvantage.com>
2019-01-30 16:18:42 -0500

    revised awards function

--- katalyst-web/Content/js/katalyst/modules/_awards.js
+++ katalyst-web/Content/js/katalyst/modules/_awards.js
@@ -6,13 +6,15 @@
 
         setInterval(function () {
             count++;
+
             $("#js-awards-name").fadeOut(400, function () {
               $(this).text(awardsArray[count]).fadeIn(400);
-
-              if(count == awardsArray.length -1){
-                  count = 0;
-              }
             });
+  
+            if(count == awardsArray.length){
+                count = 0;
+            }
+
           }, 7000);
     }
 };
