68de452e3cc06f98ec8611d834538622d237f032
Kelly Knights <kknights@katalystadvantage.com>
2019-01-30 15:43:53 -0500

    added main section js animations

--- katalyst-web/Content/js/katalyst/main.js
+++ katalyst-web/Content/js/katalyst/main.js
@@ -2,5 +2,6 @@
 $(document).ready(function () {
         headerHelper.init();
         urlNavHelper.init();
+        awardsHelper.init();
 });
 
---
+++ katalyst-web/Content/js/katalyst/modules/_awards.js
@@ -0,0 +1,20 @@
+﻿var awardsHelper = {
+    init: function(){
+        let count = 0,
+            awardsArray = ['Cannes Health Lions—Lighter Blue (Silver)', 'Etsy enamel pin la croix', 'Franzen copper mug vaporware poutine', 'craft beer hammock chicharrones'];
+
+
+        setInterval(function () {
+            count++;
+            $("#name").fadeOut(400, function () {
+              $(this).text(awardsArray[count % awardsArray.length]).fadeIn(400);
+            });
+          }, 7000);
+
+    }
+};
+
+
+
+
+
